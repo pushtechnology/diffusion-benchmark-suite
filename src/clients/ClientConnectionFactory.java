dc383a4f717c35cb6055e374537b7e107cb3feff
nitsanw <nitsanw@yahoo.com>
2013-04-17 09:59:03 +0100

    Add support for secure protocol testing

--- src/clients/ClientConnectionFactory.java
+++ src/clients/ClientConnectionFactory.java
@@ -15,12 +15,27 @@
  */
 package clients;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
 import java.util.concurrent.atomic.AtomicLong;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
 
 import com.pushtechnology.diffusion.api.ServerConnectionListener;
 import com.pushtechnology.diffusion.api.client.ExternalClientConnection;
+import com.pushtechnology.diffusion.api.connection.ConnectionFactory;
 import com.pushtechnology.diffusion.api.connection.ServerDetails;
+import com.pushtechnology.diffusion.utils.JVMSupport;
 
 class ClientConnectionFactory {
     private final static boolean LOG_EXCEPTION = Boolean.getBoolean("log.exception");
@@ -29,7 +44,7 @@ class ClientConnectionFactory {
     private final AtomicLong connectionRefusedCounter;
     private final String[] connectStrings;
     private final String[] networkInterfaces;
-
+    private SSLContext sslContext; 
     public ClientConnectionFactory(AtomicLong connectionAttemptsCounter,
     AtomicLong connectionRefusedCounter,
     String[] connectStrings,
@@ -50,9 +65,11 @@ class ClientConnectionFactory {
 
             String url = connectStrings[rotator%
                 connectStrings.length];
+            ServerDetails serverDetails;
+                serverDetails = createServerDetails(url);
             ExternalClientConnection connection =
                 new ExternalClientConnection(
-                    listener,url);
+                    listener,serverDetails);
             rotateNic(connection,rotator);
             connection.connect(topic);
             return connection;
@@ -64,6 +81,79 @@ class ClientConnectionFactory {
             connectionRefusedCounter.incrementAndGet();
             return null;
         }
+    }
+    /**
+     * Create the Diffusion ServerDetails, setting the input buffer size and SSL context if the connection is secure
+     * 
+     * @param url
+     *            Diffusion server URL
+     * @return {@link ServerDetails}
+     * @throws Throwable
+     */
+    private ServerDetails createServerDetails(final String url) throws Exception
+    {
+        final ServerDetails serverDetails = ConnectionFactory.createServerDetails(url);
+        if (serverDetails.isSecureConnection())
+        {
+            serverDetails.setSSLContext(createSSLContextWhichTrustsDiffusion());
+            serverDetails.setInputBufferSize(64 * 1024); // 64KB, to match the default in Connectors.xml
+        }
+        return serverDetails;
+    }
+    /**
+     * In order to allow tests to connect to Diffusion over SSL based protocols, the client needs to trust the server's
+     * certificate.
+     * 
+     * The client can be configured to use a specific SSLContext, this method creates an SSL certificate which is
+     * configured to trust the Diffusion certificate.
+     * 
+     * To do this, it finds Diffusion's keystore, extracts the root of the certificate chain, and puts it in a new
+     * keystore, which it then adds to a trust manager, which it uses to build an SSLContext.
+     * 
+     * @return A new SSLContext which is configured to trust the Diffusion's certificate
+     * @throws KeyStoreException
+     * @throws RuntimeException
+     * @throws IOException
+     * @throws CertificateException
+     * @throws NoSuchAlgorithmException
+     * @throws NoSuchProviderException
+     * @throws KeyManagementException
+     */
+    private SSLContext createSSLContextWhichTrustsDiffusion() throws KeyStoreException,
+            RuntimeException,
+            NoSuchAlgorithmException,
+            CertificateException,
+            IOException,
+            NoSuchProviderException,
+            KeyManagementException
+    {
+
+        if(sslContext!=null){
+            return sslContext;
+        }
+        final KeyStore diffusionKeystore = KeyStore.getInstance(KeyStore.getDefaultType());
+        final FileInputStream input = new FileInputStream(new File(new File(System.getenv("DIFFUSION_HOME"),
+                "etc"), "keystore"));
+        try
+        {
+            diffusionKeystore.load(input, null);
+        }
+        finally
+        {
+            input.close();
+        }
+        assert diffusionKeystore.containsAlias("diffusion") : "Unable to load certificate";
+        final Certificate[] chain = diffusionKeystore.getCertificateChain("diffusion");
+
+        final KeyStore clientKeystore = KeyStore.getInstance(KeyStore.getDefaultType());
+        clientKeystore.load(null);
+        // Presumably we only need to trust the last element in the certificate chain.
+        clientKeystore.setCertificateEntry("diffusion-root", chain[chain.length - 1]);
+        final TrustManagerFactory trustManagerFactory = JVMSupport.getTrustManagerFactory();
+        trustManagerFactory.init(clientKeystore);
+        sslContext = SSLContext.getInstance("SSL");
+        sslContext.init(null, trustManagerFactory.getTrustManagers(), null);
+        return sslContext;
     }
 
     void rotateNic(ExternalClientConnection connection,int rotator) {
