a00e1fed34eff6a6661ebb12264176863ef5dc07
nitsanw <nitsanw@yahoo.com>
2013-05-15 14:56:36 +0200

    some doc

--- src/clients/LatencyMonitoringClient.java
+++ src/clients/LatencyMonitoringClient.java
@@ -1,16 +1,39 @@
+/*
+ * Copyright 2013 Push Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package clients;
 
 import java.nio.ByteBuffer;
 
-import com.pushtechnology.diffusion.api.ServerConnection;
-import com.pushtechnology.diffusion.api.message.MessageException;
-import com.pushtechnology.diffusion.api.message.TopicMessage;
-import com.pushtechnology.diffusion.message.DataMessageImpl;
-
 import monitoring.ExperimentCounters;
 import monitoring.Histogram;
 
+import com.pushtechnology.diffusion.api.ServerConnection;
+import com.pushtechnology.diffusion.api.message.MessageException;
+import com.pushtechnology.diffusion.api.message.TopicMessage;
+
+/**
+ * This client will track message latency by reading the first 8 bytes of any
+ * message received and adding to it's latency histogram. The timestamp is
+ * in nano seconds.
+ * 
+ * @author nitsanw
+ *
+ */
 public class LatencyMonitoringClient extends MessageCountingClient {
+    // CHECKSTYLE:OFF
     private static final int WARMUP_MESSAGES = 20000;
     private final Histogram histogram = new Histogram(1024, 10000);
     protected ServerConnection connection;
@@ -30,7 +53,7 @@ public class LatencyMonitoringClient extends MessageCountingClient {
         }
     }
 
-    // CHECKSTYLE:OFF
+
     @Override
     public void onMessage(ServerConnection serverConnection,
             TopicMessage topicMessage) {
