9b7b4e018ba0e42a826088ad35b5785c3641eeba
mattunderscorechampion <mattunderscorechampion@gmail.com>
2014-02-14 20:22:09 +0000

    Resolved checkstyle warnings.

--- src/com/pushtechnology/benchmarks/rc/BaseRemoteListener.java
+++ src/com/pushtechnology/benchmarks/rc/BaseRemoteListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Push Technology
+ * Copyright 2013, 2014 Push Technology
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,147 +28,175 @@ import com.pushtechnology.diffusion.api.remote.RemoteServiceCloseReason;
 import com.pushtechnology.diffusion.api.remote.RemoteServiceListener;
 import com.pushtechnology.diffusion.api.topic.TopicSelector;
 
+/**
+ * Base implementation of a RemoteServiceListener.
+ */
 public class BaseRemoteListener implements RemoteServiceListener {
 
-	private CountDownLatch registrationLatch = null;
-	protected final BaseService service;
-	
-	public BaseRemoteListener(BaseService service) {
-		this.service = service;
-	}
-	
-	@SuppressWarnings("deprecation")
+    /**
+     * Registration latch.
+     */
+    private CountDownLatch registrationLatch = null;
+    /**
+     * The service the listener is for.
+     */
+    protected final BaseService service;
+
+    /**
+     * Constructor.
+     * @param service The service the listener is for.
+     */
+    public BaseRemoteListener(final BaseService service) {
+        this.service = service;
+    }
+
+    @SuppressWarnings("deprecation")
     @Override
-	public void clientConnected(ClientDetails clientDetails) {
-		Logs.fine("clientConnected(" + clientDetails.getClientID() + ")");
-	}
+    public final void clientConnected(ClientDetails clientDetails) {
+        Logs.fine("clientConnected(" + clientDetails.getClientID() + ")");
+    }
 
-	@SuppressWarnings("deprecation")
+    @SuppressWarnings("deprecation")
     @Override
-	public void clientDetailsChanged(ClientDetails clientDetails) {
-		Logs.fine("clientDetailsChanged(" + clientDetails.getClientID() + ")");
-	}
+    public final void clientDetailsChanged(ClientDetails clientDetails) {
+        Logs.fine("clientDetailsChanged(" + clientDetails.getClientID() + ")");
+    }
 
-	@SuppressWarnings("deprecation")
+    @SuppressWarnings("deprecation")
     @Override
-	public void clientDisconnected(String clientId) {
-		Logs.fine("clientDisconnected(" + clientId + ")");
-	}
+    public final void clientDisconnected(String clientId) {
+        Logs.fine("clientDisconnected(" + clientId + ")");
+    }
 
-	@SuppressWarnings("deprecation")
+    @SuppressWarnings("deprecation")
     @Override
-	public void clientFetch(ClientDetails clientDetails, String topicName, List<String> headers) {
-		Logs.fine("clientFetch(" + clientDetails.getClientID() + ", " + topicName + ")");
-		try {
-			service.getRemoteService().sendFetchReply(clientDetails.getClientID(), topicName, headers);
-		}
-		catch(APIException ex) {
-			Logs.warning("Failed to send fetch reply to client " + clientDetails.getClientID() + " for topic " + topicName);
-		}
-	}
+    public final void clientFetch(ClientDetails clientDetails,
+            String topicName, List<String> headers) {
+        Logs.fine("clientFetch(" + clientDetails.getClientID() + ", "
+                + topicName + ")");
+        try {
+            service.getRemoteService().sendFetchReply(
+                    clientDetails.getClientID(), topicName, headers);
+        } catch (APIException ex) {
+            Logs.warning("Failed to send fetch reply to client "
+                    + clientDetails.getClientID() + " for topic " + topicName);
+        }
+    }
 
-	@SuppressWarnings("deprecation")
-	@Override
-	public void clientSubscribe(ClientDetails clientDetails, String topicName) {
-		Logs.fine("clientSubscribe(" + clientDetails.getClientID() + ", " + topicName + ")");
-		try {
-			service.getRemoteService().subscribeClient(clientDetails.getClientID(), topicName);
-		}
-		catch(APIException ex) {
-			Logs.warning("Failed to subscribe client " + clientDetails.getClientID() + " to topic " + topicName, ex);
-		}
-	}
-
-	@SuppressWarnings("deprecation")
-	@Override
-	public void clientSubscribe(ClientDetails clientDetails, TopicSelector topicSelector) {
-		Logs.fine("clientSubscribe(" + clientDetails.getClientID() + ", " + topicSelector + ")");
-	}
-
-	@SuppressWarnings("deprecation")
-	@Override
-	public void clientUnsubscribe(String clientId, String topicName, boolean hasSubscribers) {
-		Logs.fine("clientUnSubscribe(" + clientId + ", " + topicName);
-	}
-
-	@SuppressWarnings("deprecation")
-	@Override
-	public void closed(RemoteServiceCloseReason reason) {
-		Logs.fine("closed(" + reason + ")");
-	}
-
-	@SuppressWarnings("deprecation")
-	@Override
-	public void messageFromClient(ClientDetails clientDetails, String topicName,
-			TopicMessage message) {
-		Logs.fine("messageFromClient(" + clientDetails.getClientID() + ", " + topicName + ")");
-	}
-
-	@SuppressWarnings("deprecation")
+    @SuppressWarnings("deprecation")
     @Override
-	public void messageFromPublisher(TopicMessage message) {
-		Logs.fine("messageFromPublisher(" + message.getTopicName() + ")");
-	}
+    public final void clientSubscribe(ClientDetails clientDetails,
+            String topicName) {
+        Logs.fine("clientSubscribe(" + clientDetails.getClientID() + ", "
+                + topicName + ")");
+        try {
+            service.getRemoteService().subscribeClient(
+                    clientDetails.getClientID(), topicName);
+        } catch (APIException ex) {
+            Logs.warning(
+                    "Failed to subscribe client " + clientDetails.getClientID()
+                            + " to topic " + topicName, ex);
+        }
+    }
 
-	@SuppressWarnings("deprecation")
-	@Override
-	public void registerFailed(String errorMessage) {
-		if(registrationLatch != null) {
-			registrationLatch.countDown();
-		}
-		Logs.warning("Registration failed: " + errorMessage);
-	}
+    @SuppressWarnings("deprecation")
+    @Override
+    public final void clientSubscribe(ClientDetails clientDetails,
+            TopicSelector topicSelector) {
+        Logs.fine("clientSubscribe(" + clientDetails.getClientID() + ", "
+                + topicSelector + ")");
+    }
 
-	@SuppressWarnings("deprecation")
-	@Override
-	public void registered() {
-		if(registrationLatch != null) {
-			registrationLatch.countDown();
-		}
-		Logs.fine("Registered");
-	}
+    @SuppressWarnings("deprecation")
+    @Override
+    public final void clientUnsubscribe(String clientId, String topicName,
+            boolean hasSubscribers) {
+        Logs.fine("clientUnSubscribe(" + clientId + ", " + topicName);
+    }
 
-	@SuppressWarnings("deprecation")
-	@Override
-	public void serviceRequest(RemoteRequest request) {
-		Logs.fine("serviceRequest(" + request + ")");
+    @SuppressWarnings("deprecation")
+    @Override
+    public final void closed(RemoteServiceCloseReason reason) {
+        Logs.fine("closed(" + reason + ")");
+    }
 
-	}
+    @SuppressWarnings("deprecation")
+    @Override
+    public void messageFromClient(ClientDetails clientDetails,
+            String topicName,
+            TopicMessage message) {
+        Logs.fine("messageFromClient(" + clientDetails.getClientID() + ", "
+                + topicName + ")");
+    }
 
-	@SuppressWarnings("deprecation")
-	@Override
-	public void topicAddFailed(String topicName, String errorMessage) {
-		Logs.warning("topicAddFailed(" + topicName + ") : " + errorMessage);
-	}
+    @SuppressWarnings("deprecation")
+    @Override
+    public final void messageFromPublisher(TopicMessage message) {
+        Logs.fine("messageFromPublisher(" + message.getTopicName() + ")");
+    }
 
-	@SuppressWarnings("deprecation")
-	@Override
-	public void topicSubscribeFailed(String clientId, String topicName, String errorMessage) {
-		Logs.warning("topicSubscribeFailed(" + clientId + ", " + topicName + ") : " + errorMessage);
-	}
+    @SuppressWarnings("deprecation")
+    @Override
+    public final void registerFailed(String errorMessage) {
+        if (registrationLatch != null) {
+            registrationLatch.countDown();
+        }
+        Logs.warning("Registration failed: " + errorMessage);
+    }
 
-	/**
-	 * Set up a latch for synchronous registration notifications.
-	 */
-	public void resetRegisterLatch() {
-		registrationLatch = new CountDownLatch(1);
-	}
-	
-	/**
-	 * Synchronous wait for registration notifications.
-	 * 
-	 * @param timeout
-	 * @param unit
-	 */
-	public void waitForRegistration(long timeout, TimeUnit unit) {
-		if(registrationLatch == null) {
-			throw new IllegalStateException("Registration Latch not initialised");
-		}
-		
-		try {
-			registrationLatch.await(timeout, unit);
-		}
-		catch(InterruptedException ignore) {
-		}
-	}
+    @SuppressWarnings("deprecation")
+    @Override
+    public final void registered() {
+        if (registrationLatch != null) {
+            registrationLatch.countDown();
+        }
+        Logs.fine("Registered");
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public final void serviceRequest(RemoteRequest request) {
+        Logs.fine("serviceRequest(" + request + ")");
+
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public final void topicAddFailed(String topicName, String errorMessage) {
+        Logs.warning("topicAddFailed(" + topicName + ") : " + errorMessage);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public final void topicSubscribeFailed(String clientId, String topicName,
+            String errorMessage) {
+        Logs.warning("topicSubscribeFailed(" + clientId + ", " + topicName
+                + ") : " + errorMessage);
+    }
+
+    /**
+     * Set up a latch for synchronous registration notifications.
+     */
+    public final void resetRegisterLatch() {
+        registrationLatch = new CountDownLatch(1);
+    }
+
+    /**
+     * Synchronous wait for registration notifications.
+     * 
+     * @param timeout The timeout period.
+     * @param unit The timeout unit.
+     */
+    public final void waitForRegistration(final long timeout,
+            final TimeUnit unit) {
+        if (registrationLatch == null) {
+            throw new IllegalStateException(
+                    "Registration Latch not initialised");
+        }
+
+        try {
+            registrationLatch.await(timeout, unit);
+        } catch (final InterruptedException ignore) {
+        }
+    }
 }
